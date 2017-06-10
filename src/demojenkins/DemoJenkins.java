8f3529c1b66717a0a16d3b41ebdb675f859df8a3
Aparna-7 <aparnanarayanan@ymail.com>
2017-06-10 18:02:41 +0530

    1st commit

---
+++ src/demojenkins/DemoJenkins.java
@@ -0,0 +1,15 @@
+package demojenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkins {
+
+		@Test
+		public void testJenkins(){
+			
+			System.out.println("Welcome to Jenkins World");
+			
+		}
+
+	}
+
